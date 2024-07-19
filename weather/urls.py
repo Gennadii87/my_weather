155701ed7a0253a8caeb9c30a2dfc3b1f15f8af1
Gennadii87 <Gennadii_s@inbox.ru>
2024-07-19 23:57:56 +1000

    update code

--- weather/urls.py
+++ weather/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+
 from . import views
+from .api_views import CitySearchViewSet, CityAllSearchViewSet
 from .service import autocompletion
+
+router = DefaultRouter()
+router.register(r'history', CityAllSearchViewSet, basename='city_search')
+router.register(r'history', CitySearchViewSet, basename='city_search')
 
 urlpatterns = [
     path('', views.weather_view, name='weather'),
@@ -9,4 +16,6 @@ urlpatterns = [
     path('autocompletion/', autocompletion, name='autocompletion'),
     path('request_count/', views.city_request_count, name='request_count'),
     path('all_request_counts/', views.all_city_request_counts, name='all_request_counts'),
+
+    path("api/", include(router.urls)),
 ]
