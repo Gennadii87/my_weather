434e1be4372b12cb967a4c0787147cae3e92c14c
Gennadii87 <Gennadii_s@inbox.ru>
2024-07-22 02:25:51 +1000

    updtate

--- weather/service.py
+++ weather/service.py
@@ -19,16 +19,16 @@ class CityNotFoundError(Exception):
 def get_coordinates(city_name):
     geolocator = Nominatim(user_agent='weather_app')
     location = geolocator.geocode(city_name)
-    print(location)
     if location is None:
         raise CityNotFoundError(f'Город «{city_name}» не найден.')
-    return {'latitude': location.latitude, 'longitude': location.longitude}
+    return {'latitude': location.latitude, 'longitude': location.longitude, 'location': location}
 
 
 @cache_memoize(30)
 def get_weather(city):
     try:
         coordinates = get_coordinates(city)
+        location = coordinates.get('location')
     except CityNotFoundError as e:
         return None, str(e)
     url = f"https://api.open-meteo.com/v1/forecast"
@@ -41,8 +41,12 @@ def get_weather(city):
         "past_days": 0,
         "forecast_days": 3
     }
+
     response = requests.get(url, params=params)
-    return response.json(), None
+    weather_data = response.json()
+    weather_data['location'] = location
+
+    return weather_data, None
 
 
 @require_GET
